9a0db1aed20be5a55b8786ae90ac35e7a1b0f955
sailymarudwar <sayali.m28@gmail.com>
2020-12-17 16:23:44 -0600

    wefwefewfwef

--- src/app/interceptors/add-credentials.interceptor.ts
+++ src/app/interceptors/add-credentials.interceptor.ts
@@ -5,18 +5,22 @@ import {
   HttpEvent,
   HttpInterceptor
 } from '@angular/common/http';
+import {CookieService} from 'ngx-cookie-service';  
+
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class AddCredentialsInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(private coo:CookieService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
 
     request = request.clone({
-      withCredentials: true
+      withCredentials: true,
+      headers: request.headers.set('Authorization',this.coo.get('email'))
     });
+   
 
     return next.handle(request);
   }
